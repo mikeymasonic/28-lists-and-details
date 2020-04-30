1aab9a4306b441109d857fb03453e69fcce94e6b
Michael Romay <amerror@gmail.com>
2020-04-29 19:18:43 -0700

    list clean up

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const List = ({ name, occupation, status, image }) => (
-  <ul key={name}>
-    <li><Link to={`detail/${name}`} >{name}</Link></li>
-    <li>{occupation}</li>
-    <li>{status}</li>
-    <img src={image} />
-  </ul>
+  <section>
+    <p><Link to={`detail/${name}`} >{name}</Link></p>
+    {occupation && <p>Occupation: {occupation}</p>}
+    {status && <p>Status: {status}</p>}
+    {image && <img src={image} />}
+  </section>
 );
 
 List.propTypes = {

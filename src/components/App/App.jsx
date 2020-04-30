ee70120bd913b84c2922036f2d01f98d68f214a8
Michael Romay <amerror@gmail.com>
2020-04-29 18:10:49 -0700

    CharaterList

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
-import XFilesViewer from '../../container/XFilesViewer';
+import { 
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+
+import Header from '../Header/Header';
+import CharacterList from '../../container/CharacterList/CharacterList';
 
 export default function App() {
-  return <XFilesViewer />;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/:page?" component={CharacterList} />
+        {/* <Route exact path="/detail/:id?" component={Charac} /> */}
+      </Switch>
+    </Router>
+  );
 }
-  
---
+++ src/container/CharacterList/CharacterList.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import List from '../../components/List/List';
+import { fetchList } from '../../services/xFilesAPI';
+import { useParams } from 'react-router-dom';
+
+const CharacterList = () => {
+  const [characters, setCharacters] = useState([]);
+  let { page } = useParams();
+  if(!page) page = 1;
+
+  useEffect(() => {
+    fetchList(page)
+      .then(apiCharacters => setCharacters(apiCharacters));
+  }, []);
+
+  const characterNodes = characters.map(character => {
+    return <List key={character.name} {...character} />;
+  });
+  
+  return (
+    <>
+      {characterNodes}
+    </>
+  );
+};
+
+export default CharacterList;

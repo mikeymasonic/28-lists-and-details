b6ac309edf80b9e380ecc9e9bb7b36e893b1e3d6
Michael Romay <amerror@gmail.com>
2020-04-29 19:26:52 -0700

    paging

--- src/container/CharacterList/CharacterList.jsx
+++ src/container/CharacterList/CharacterList.jsx
@@ -1,17 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import List from '../../components/List/List';
 import { fetchList } from '../../services/xFilesAPI';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 
 const CharacterList = () => {
   const [characters, setCharacters] = useState([]);
   let { page } = useParams();
+  let history = useHistory();
+
   if(!page) page = 1;
 
   useEffect(() => {
     fetchList(page)
       .then(apiCharacters => setCharacters(apiCharacters));
   }, [page]);
+
+  const nextPage = () => {
+    const newNum = Number(page) + Number(event.target.value);  
+    if(newNum > 0) {
+      page = newNum;
+      history.push(`/${page}`);
+    }
+  };
 
   const characterNodes = characters.map(character => {
     return <List key={character.name} {...character} />;
@@ -20,6 +30,10 @@ const CharacterList = () => {
   return (
     <>
       {characterNodes}
+      <div>
+        <button value={-1} onClick={nextPage}>Back</button>
+        <button value={1} onClick={nextPage}>Next</button>
+      </div>
     </>
   );
 };
